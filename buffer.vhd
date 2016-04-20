d246b8fd2f5f41b29791d1de23e50500207ff3ed
Luke Boudreau <Luke Boudreau>
2016-04-20 16:15:24 -0400

    buffer in progress

--- buffer.vhd
+++ buffer.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity buffe is generic (n: in positive);
+entity buffer is generic (n: in positive := 4120);
     port(
         bit     : in std_logic;                   --Bit to shift in
         En      : in std_logic;                       --Boolean to shift or hold
@@ -24,8 +24,20 @@ component flipflop is
             clk : in std_logic;
             Q : out std_logic );
 end component;
+
+-- These are the wires to the output values
+signal pixel_lines : std_logic_vector(71 downto 0);
+register : array(0 to n);
+-- Used to index pixel_lines
+variable count : integer := 0;
+
 begin
     dflips: for i in n-1 to 0 generate
+
+        First_row_pixels : if( i >= 0 and i <= 23) generate
+            flips : flipflop
+                port map(D => );
+        end generate First_row_pixels;
 
     end generate dflips;
 end architecture;
